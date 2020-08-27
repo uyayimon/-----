8293e5ce55785159a4de82d6dfeb35087822e219
uyayimon <mmjhmk@gmail.com>
2020-08-27 19:52:55 +0900

    script更新

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let q1 = document.getElementById('q1'); let q2 = document.getElementById('q2'); let q3 = document.getElementById('q3'); let q4 = document.getElementById('q4'); let q5 = document.getElementById('q5');
 let kan = document.querySelector('.kan-wrapper');
+let han1 = document.getElementById('1han');
 
 let fu_result = document.getElementById('fu_result');
 let han_result = document.getElementById('han-result');
@@ -7,60 +8,64 @@ let point = document.getElementById('ten-result');
 let pointmove = document.getElementById('ten-idou');
 
 // 選択肢による項目のアクティブ・パッシブ化
-q1.addEventListener('change', function () {
-  if (document.getElementById('q1-yes').checked === true) {
-    q2.classList.remove('disabled');
-    q3.classList.add('disabled');
-    q4.classList.add('disabled');
-    q5.classList.add('disabled');
-  }
-  else if (document.getElementById('q1-no').checked === true) {
-    q3.classList.remove('disabled');
-    q2.classList.add('disabled');
-  }
-});
-q2.addEventListener('change', function () {
-  if (document.getElementById('q2-yes').checked === true) {
-    if (document.getElementById('1han').checked = true) {
-      document.getElementById('1han').disabled = true;
-      document.getElementById('2han').checked = true;
-      han_su = 2;
+let Y_N = document.querySelectorAll('.Y-N');
+for (i = 0; i < Y_N.length; i++){
+  Y_N[i].addEventListener('change', function () {
+    if (document.getElementById('q1-yes').checked === true) {
+      q2.classList.remove('disabled');
+      q3.classList.add('disabled');
+      q4.classList.add('disabled');
+      q5.classList.add('disabled');
     }
-  }
-  if (document.getElementById('q2-no').checked === true) {
-    document.getElementById('1han').disabled = false;
-  }
-});
-q3.addEventListener('change', function () {
-  if (document.getElementById('q3-yes').checked === true) {
-    q4.classList.add('disabled');
-    q5.classList.add('disabled');
-    kan.classList.add('disabled');
-    document.getElementById('1han').disabled = true;
-    document.getElementById('2han').checked = true;
-    document.getElementById('2han').checked = true;
-  }
-  else if (document.getElementById('q3-no').checked === true) {
-    q4.classList.remove('disabled');
-    q5.classList.remove('disabled');
-    kan.classList.remove('disabled');
-    document.getElementById('1han').disabled = false;
-  }
-});
-q4.addEventListener('change', function () {
-  if (document.getElementById('q4-yes').checked === true) {
-    kan.classList.add('disabled');
-  }
-  else if (document.getElementById('q4-no').checked === true) {
-    kan.classList.remove('disabled');
-  }
-});
+    if (document.getElementById('q1-no').checked === true) {
+      q3.classList.remove('disabled');
+      q2.classList.add('disabled');
+    }
+    if (document.getElementById('q2-yes').checked === true) {
+      if (han1.checked = true) {
+        han1.disabled = true;
+        document.getElementById('2han').checked = true;
+        han_su = 2;
+      }
+    }
+    if (document.getElementById('q2-no').checked === true) {
+      han1.disabled = false;
+    }
+    if (document.getElementById('q3-yes').checked === true) {
+      q4.classList.add('disabled');
+      q5.classList.add('disabled');
+      kan.classList.add('disabled');
+      han1.disabled = true;
+      document.getElementById('2han').checked = true;
+      document.getElementById('2han').checked = true;
+    }
+    if (document.getElementById('q3-no').checked === true) {
+      q4.classList.remove('disabled');
+      q5.classList.remove('disabled');
+      kan.classList.remove('disabled');
+      han1.disabled = false;
+    }
+    if (document.getElementById('q4-yes').checked === true) {
+      kan.classList.add('disabled');
+    }
+    if (document.getElementById('q4-no').checked === true) {
+      kan.classList.remove('disabled');
+    }
+    if (document.getElementById('q5-yes').checked === true) {
+      if (han1.checked = true) {
+        han1.disabled = true;
+        document.getElementById('2han').checked = true;
+        han_su = 2;
+      }
+    }
+  });
+}
 
 // 槓子の有無と値取得
 let ankan1Count = 0; let ankan2Count = 0; let minkan1Count = 0; let minkan2Count = 0; // 初期値
 let limit = 4;
 let ankan1 = document.getElementById('ankan1'); let ankan2 = document.getElementById('ankan2'); let minkan1 = document.getElementById('minkan1'); let minkan2 = document.getElementById('minkan2');
-let kansCount = document.querySelectorAll('.count-value');
+// let kansCount = document.querySelectorAll('.count-value');
 
 document.getElementById('ankan1+').addEventListener('click', function () {
   if (ankan1Count < limit) { ankan1Count++; ankan1.innerHTML = ankan1Count; }
@@ -120,14 +125,20 @@ document.getElementById('calculation').addEventListener('click', function () {
 
   let kan_fu = ankan1Count * 30 + ankan2Count * 20 + minkan2Count * 20 + minkan2Count * 10;
 
-  fu_su += kan_fu;
+  if (document.getElementById('q3-yes').checked === true || document.getElementById('q4-yes').checked === true){
+    kan_fu = 0;
+    fu_su += kan_fu;
+  }
+  else {
+    fu_su += kan_fu;
+  }
   fu_result.innerHTML = fu_su;
   han_result.innerHTML = han_su;
 
   if (document.getElementById('oya').checked === true) {
     if (han_su == 1) {
-      if (fu_su == 20) { alert('20符1翻は平和ツモのみです'); return; }
-      if (fu_su == 25) { alert('25符は七対子のみです'); return; }
+      if (fu_su == 20) { alert('20符1翻のアガりはありません'); return; }
+      if (fu_su == 25) { alert('25符1翻のアガりはありません'); return; }
       if (fu_su == 30) { point.innerHTML = '1500'; pointmove.innerHTML = '(500オール)'; }
       if (fu_su == 40) { point.innerHTML = '2000'; pointmove.innerHTML = '(700オール)'; }
       if (fu_su == 50) { point.innerHTML = '2400'; pointmove.innerHTML = '(800オール)'; }
@@ -174,8 +185,8 @@ document.getElementById('calculation').addEventListener('click', function () {
 
   if (document.getElementById('ko').checked === true) {
     if (han_su == 1) {
-      if (fu_su == 20) { alert('20符1翻は平和ツモのみです'); return; }
-      if (fu_su == 25) { alert('25符は七対子のみです'); return; }
+      if (fu_su == 20) { alert('20符1翻のアガりはありません'); return; }
+      if (fu_su == 25) { alert('25符1翻のアガりはありません'); return; }
       if (fu_su == 30) { point.innerHTML = '1000'; pointmove.innerHTML = '(300,500)'; }
       if (fu_su == 40) { point.innerHTML = '1300'; pointmove.innerHTML = '(400,700)'; }
       if (fu_su == 50) { point.innerHTML = '1600'; pointmove.innerHTML = '(400,800)'; }
@@ -237,7 +248,7 @@ document.getElementById('reset').addEventListener('click', function () {
   // 初期値に戻す
   fu_su = 0;
   ankan1Count = 0; ankan2Count = 0; minkan1Count = 0; minkan2Count = 0;
-  document.getElementById('1han').checked = true;
+  han1.checked = true;
   document.getElementById('ko').checked = true;
   kan.classList.remove('disabled');
   // 槓子の値を戻す
