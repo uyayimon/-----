133913c439bca4f9703efd74857190ef6e914529
uyayimon <mmjhmk@gmail.com>
2021-06-06 16:42:44 +0900

    sw修正

--- service_worker.js
+++ service_worker.js
@@ -1,26 +1,9 @@
-const CACHE_NAME = 'mic-caches-v1';
-const urlsToCache = [
-  '/',
-  'style.css',
-  'main.js'
-];
-
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then(function (cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener("install", function (e) {
+  console.log("[ServiceWorker] Install");
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(function (response) {
-        return response ? response : fetch(event.request);
-      })
-  );
+self.addEventListener("activate", function (e) {
+  console.log("[ServiceWorker] Activate");
 });
+
+self.addEventListener("fetch", function (event) { });
