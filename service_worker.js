f05e8c547c437a5c45e63727e3df4d0822b0b205
uyayimon <mmjhmk@gmail.com>
2021-06-06 16:31:14 +0900

    head内修正

--- service_worker.js
+++ service_worker.js
@@ -1,6 +1,8 @@
 const CACHE_NAME = 'mic-caches-v1';
 const urlsToCache = [
-  /uyayimon.github.io/,
+  '/',
+  'style.css',
+  'main.js'
 ];
 
 self.addEventListener('install', function (event) {
@@ -8,7 +10,7 @@ self.addEventListener('install', function (event) {
     caches
       .open(CACHE_NAME)
       .then(function (cache) {
-        return cache.addAll(urlsToCache.map(url => new Request(url, { credentials: 'same-origin' })));
+        return cache.addAll(urlsToCache);
       })
   );
 });
